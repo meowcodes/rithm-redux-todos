1ab1a8400fb09f84f43f43a57b9419cff3ce33e9
Stéphane Dryefus <floatingrock@gmail.com>
2019-04-01 18:15:23 -0700

    Add start of tests.:

---
+++ src/rootReducer.test.js
@@ -0,0 +1,6 @@
+import rootReducer from './rootReducer';
+
+it('Rerturns an appropriate object depdening on action type', () => {
+    const addAction = {type: "ADD", payload: {text: "test", id: "first"}};
+    
+});
